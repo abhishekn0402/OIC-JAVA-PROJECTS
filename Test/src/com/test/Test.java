87504e8728055ea177cd3ca8c4436d32583a97fb
Abhishek Nagaraj <Abhishek Nagaraj@LAPTOP-8SOHCLNU.mshome.net>
2021-08-22 12:37:13 +0530

    test

---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Test 
+{	
+	
+public static void main(String[] args) throws IOException
+{
+	 String host = "02cb1a2.netsolhost.com";
+	 String user = "starlinks";
+	 String pass = "$#@Starlinks";
+	 int port = 21;
+
+}
+}
